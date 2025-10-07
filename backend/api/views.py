184a4ce39f2fbba781b672e69a192eb99a061ae3
Rashu <shardarashu@gmail.com>
2025-10-06 20:11:58 -0400

    Remove files related to backend to facilitate merging.

--- backend/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-@api_view(["GET"])
-def get_message(request):
-    with open("data.txt", "r") as f:
-        message = f.read().strip()
-    return Response({"message": message})
-
