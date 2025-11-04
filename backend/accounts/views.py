8d07a95d59f7b884bc27935eb61d05947bf74f18
IlyaK27 <ilyakononov2714@gmail.com>
2025-11-04 00:42:36 -0500

    Moved account checking from views to services
    Cleaned up views by moving non visualizing code to services instead

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,18 +1,14 @@
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-
-from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
-from rest_framework_simplejwt.tokens import RefreshToken
+from .services import register_user, login_user
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def protected_view(request):
     return Response({"message": f"Hello, {request.user.username}!"})
-    
+
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def register(request):
@@ -20,19 +16,8 @@ def register(request):
     email = request.data.get("email")
     password = request.data.get("password")
 
-    if not username or not email or not password:
-        return Response({"error": "All fields are required."}, status=status.HTTP_400_BAD_REQUEST)
-
-    if User.objects.filter(username=username).exists():
-        return Response({"error": "Username already exists."}, status=status.HTTP_400_BAD_REQUEST)
-
-    if User.objects.filter(email=email).exists():
-        return Response({"error": "Email already exists."}, status=status.HTTP_400_BAD_REQUEST)
-
-    user = User.objects.create_user(username=username, email=email, password=password)
-    user.save()
-    return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
-
+    data, status_code = register_user(username, email, password)
+    return Response(data, status=status_code)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -40,13 +25,5 @@ def login(request):
     username = request.data.get("username")
     password = request.data.get("password")
 
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        refresh = RefreshToken.for_user(user)
-        return Response({
-            "access": str(refresh.access_token),
-            "refresh": str(refresh),
-            "username": user.username,
-        })
-    else:
-        return Response({"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
+    data, status_code = login_user(username, password)
+    return Response(data, status=status_code)
