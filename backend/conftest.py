9961355531b59f8a4dd2416f76e89b6b371bcc68
Ethan Qiu <ethanqiu@gmail.com>
2025-11-05 20:28:55 -0500

    Update conftest.py

--- backend/conftest.py
+++ backend/conftest.py
@@ -3,7 +3,8 @@ import django
 from django.conf import settings
 
 # Configure Django settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 
 def pytest_configure():
     """Configure Django for pytest."""
