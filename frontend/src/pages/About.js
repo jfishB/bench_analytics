3f633c89cb245fe37e09d6757063c578d4b37929
Rashu <shardarashu@gmail.com>
2025-11-06 07:53:35 -0500

    Remove unused variables

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card"
 
 export function About() {
   const teamMembers = [
