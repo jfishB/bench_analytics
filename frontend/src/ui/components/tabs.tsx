c15726ead3cc82bca801719e227e7ac5dcdb1e43
Rashu <shardarashu@gmail.com>
2025-11-11 10:52:19 -0500

    Fix import error and 2 tailwind warnings.

--- frontend/src/ui/components/tabs.tsx
+++ frontend/src/ui/components/tabs.tsx
@@ -23,15 +23,15 @@ function TabsList({
   ...props
 }: React.ComponentProps<typeof TabsPrimitive.List>) {
   return (
-    <div className={cn("relative w-full")}> 
+    <div className={cn("relative w-full")}>
       {/* thin grey line at the very top */}
       <div className="absolute left-0 top-0 w-full h-[2px] bg-gray-200 pointer-events-none rounded-full" />
 
       <TabsPrimitive.List
         data-slot="tabs-list"
         className={cn(
-          "mt-2 bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-xl p-[3px] flex relative",
-          className,
+          "mt-2 bg-muted text-muted-foreground flex h-9 w-fit items-center justify-center rounded-xl p-[3px] relative",
+          className
         )}
         {...props}
       >
@@ -50,7 +50,7 @@ function TabsTrigger({
       data-slot="tabs-trigger"
       className={cn(
         "data-[state=active]:bg-primary data-[state=active]:text-white focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-full border border-transparent px-4 py-1 text-sm font-medium whitespace-nowrap transition-colors duration-150 focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-        className,
+        className
       )}
       {...props}
     />
