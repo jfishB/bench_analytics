34bee1ed665891a946c86e1a11044ea354ac1aaa
Lea Josephine <flgng@posteo.de>
2025-11-12 22:59:18 -0500

    added missing imports

--- backend/roster/views.py
+++ backend/roster/views.py
@@ -1,3 +1,6 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 from rest_framework import viewsets
 
 from .models import Player, Team
