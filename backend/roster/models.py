15f9b73ece06ba29a349d033b67e56144f57f1ae
IlyaK27 <ilyakononov2714@gmail.com>
2025-11-15 22:18:54 -0500

    Further update to csvs and models
    Adjusted again to include additional required data

--- backend/roster/models.py
+++ backend/roster/models.py
@@ -26,6 +26,7 @@ class Player(models.Model):
     # Stat explanations from: https://baseballsavant.mlb.com/leaderboard/custom?year=2025&type=batter&filter=&min=q&selections=pa%2Chome_run%2Ck_percent%2Cbb_percent%2Cslg_percent%2Con_base_percent%2Cisolated_power%2Cr_total_stolen_base%2Cwoba%2Cxwoba%2Cbarrel_batted_rate%2Chard_hit_percent%2Csprint_speed&chart=false&x=pa&y=pa&r=no&chartType=beeswarm&sort=1&sortDir=desc
     savant_player_id = models.PositiveIntegerField(null=True, blank=True)  # External player_id
     year = models.PositiveIntegerField(null=True, blank=True)  # Season year (e.g., 2025)
+    ab = models.PositiveIntegerField(null=True, blank=True)  # At bat count
     pa = models.PositiveIntegerField(null=True, blank=True)  # Plate appearances
     hit = models.PositiveIntegerField(null=True, blank=True)  # Plate appearances
     single = models.PositiveIntegerField(null=True, blank=True)  # Singles
@@ -44,9 +45,15 @@ class Player(models.Model):
     isolated_power = models.FloatField(
         null=True, blank=True
     )  # Shows extra bases per at-bat (pure power) - ISO = SLG - AVG | (AVG = H / AB)
-    r_total_stolen_base = models.FloatField(null=True, blank=True)  # Stolen bases - Count of successful stolen base attempts
-    b_game = models.FloatField(null=True, blank=True)  # Games played - Total games played in a season
-    b_hit_by_pitch = models.FloatField(null=True, blank=True)  # A hit-by-pitch occurs when a batter is struck by a pitched ball without swinging at it.
+    b_total_bases = models.FloatField(null=True, blank=True)  # TB: Total bases - 1 x Singles + 2 x Doubles + 3 x Triples + 4 x Home Runs
+    r_total_caught_stealing = models.FloatField(null=True, blank=True)  # CS: Caught Stealing - When a runner is put out by the defense while attempting to steal a base
+    r_total_stolen_base = models.FloatField(null=True, blank=True)  # SB: Stolen bases - Count of successful stolen base attempts
+    b_game = models.FloatField(null=True, blank=True)  # G: Games played - Total games played in a season
+    b_gnd_into_dp = models.FloatField(null=True, blank=True)  # GIPD: Grounded Into Double Play - Counts the times a batter hits a ground ball that results in two outs in one continuous play
+    b_hit_by_pitch = models.FloatField(null=True, blank=True)  # HPB: A hit-by-pitch occurs when a batter is struck by a pitched ball without swinging at it.
+    b_intent_walk = models.FloatField(null=True, blank=True)  # IBB: Intentional Base on Balls / Intentional walk - A strategic play where the defending team deliberately allows a batter to reach first base without having to swing at a pitch.
+    b_sac_fly = models.FloatField(null=True, blank=True)  # SF: Sacrifice Fly - Occurs when a batter hits a deep fly ball that is caught by an outfielder (or an infielder playing in the outfield), but a baserunner on third base tags up and scores before the play is over.
+    b_total_sacrifices = models.FloatField(null=True, blank=True)  # SH: Sacrfice hit: Occurs when a player is successful in his attempt to advance a runner (or multiple runners) at least one base with a bunt.
     woba = models.FloatField(
         null=True, blank=True
     )  # Weighted On-Base Average: Weights each event by its average run value (Weights vary slightly each season) - wOBA = ((0.69*uBB) + (0.72*HBP) + (0.89*1B) + (1.27*2B) + (1.62*3B) + (2.10*HR)) / (AB + BB - IBB + SF + HBP)
