8a35a980e54e2142f0d7807cd0b06202711e8229
Jeevesh Balendra <jeeveshbalendra@gmail.com>
2025-11-18 11:40:07 -0500

    add domain layer hooks for lineup business logic

---
+++ frontend/src/features/lineup/hooks/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Central export file for all lineup-related custom hooks.
+ * Following clean architecture principles for better state management.
+ */
+
+export { useRosterData } from "./useRosterData";
+export { usePlayerSelection } from "./usePlayerSelection";
+export { useLineupCreation } from "./useLineupCreation";
+export { useManualLineup } from "./useManualLineup";
+export { useSabermetricsLineup } from "./useSabermetricsLineup";
+export { useSavedLineups } from "./useSavedLineups";
+
