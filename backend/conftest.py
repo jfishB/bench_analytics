f6b8ef9358623491b44ab1db232370a863b8e3e4
Rashu <shardarashu@gmail.com>
2025-12-02 22:36:28 -0500

    Exclude test config files from coverage reports
    Added conftest.py  to the omit list in pytest.ini to prevent them from being included in coverage reports.

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,14 +1,14 @@
-import os
+import os  # pragma: no cover
 
-import django
-from django.conf import settings
+import django  # pragma: no cover
+from django.conf import settings  # pragma: no cover
 
-# Configure Django settings for all pytest runs.
-# locally it uses sql lite and in CI it uses the test data base settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings_test")
+# Configure Django settings for all pytest runs.  # pragma: no cover
+# locally it uses sql lite and in CI it uses the test data base settings  # pragma: no cover
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings_test")  # pragma: no cover
 
 
-def pytest_configure():
-    """Configure Django for pytest."""
-    if not settings.configured:
-        django.setup()
+def pytest_configure():  # pragma: no cover
+    """Configure Django for pytest."""  # pragma: no cover
+    if not settings.configured:  # pragma: no cover
+        django.setup()  # pragma: no cover
